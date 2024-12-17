2f086810b73b73b12b31f59dafa3e8de95cb11e1
MadForCoding <hnishad020@gmail.com>
2024-12-17 11:16:41 +0530

    Added untracked files

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\KiteController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/kite/login', 'KiteController@redirectToKite');
+Route::get('/kite/callback', 'KiteController@handleCallback');
